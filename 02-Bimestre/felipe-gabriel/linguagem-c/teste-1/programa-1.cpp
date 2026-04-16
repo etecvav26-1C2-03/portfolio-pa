6d121a7773163d973c270e42055d825fb69a67d7
FeLaLost <gabrielfernandesbarbarini@gmail.com>
2026-04-16 15:10:35 -0300

    oi

--- 02-Bimestre/felipe-gabriel/linguagem-c/teste-1/programa-1.cpp
+++ 02-Bimestre/felipe-gabriel/linguagem-c/teste-1/programa-1.cpp
@@ -1,9 +1,19 @@
-
-// Your First C++ Program
-
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World!";
+int main()
+{
+    int a = 5, b = 10, temp;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
     return 0;
 }
