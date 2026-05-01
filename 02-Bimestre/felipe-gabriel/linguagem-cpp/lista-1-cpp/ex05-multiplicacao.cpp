9a62c0550c7fec1cb83b91cbe518d2a93a986b81
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-01 16:39:54 -0300

    Update ex05-multiplicacao.cpp

--- 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex05-multiplicacao.cpp
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex05-multiplicacao.cpp
@@ -1,1 +1,23 @@
-a
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: multiplicacao.cpp
+* Data: 01/05/2026
+* Autor: Gabriel Fernandes Barbarini
+* Descrição:
+* Ler um número para a parada final e outro número que representa um múltiplo.
+* Exibir os múltiplos do número lido de 1 até o número final lido.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int lim, mult;
+    
+    cout << "Escreva o número multiplicador: ";
+    cin >> mult;
+
+    cout << "Escreva o limite: ";
+    cin >> lim;
+
