bb5a33d450b0b6d982885eeab242494e4e7381fd
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-01 16:05:12 -0300

    Update ex02-potencias.cpp

--- 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex02-potencias.cpp
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex02-potencias.cpp
@@ -1,1 +1,21 @@
-a
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: potencias.cpp
+* Data: 01/05/2026
+* Autor: Gabriel Fernandes Barbarini
+* Descrição: 
+* Ler um número inteiro, calcular e exibir separadamente 
+* as potências de 2^0 até 2^(esse número).
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    long long res = 1;
+    
+    cout << "escreva um numero: ";
+    cin >> num;
