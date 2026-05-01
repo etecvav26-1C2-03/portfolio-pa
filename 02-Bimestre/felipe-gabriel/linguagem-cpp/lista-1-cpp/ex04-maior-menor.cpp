62b7fa1d6f3ce9130cbbf1ccbecc6d1a5c5f4868
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-01 16:31:36 -0300

    Update ex04-maior-menor.cpp

--- 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex04-maior-menor.cpp
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex04-maior-menor.cpp
@@ -1,1 +1,25 @@
-a
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: menor-maior.cpp
+* Data: 01/05/2026
+* Autor: Gabriel Fernandes Barbarini
+* Descrição: 
+* Ler 10 números. Verificar e exibir o menor 
+* e maior número dessa sequência.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, maior, menor;
+    
+    for (int i = 0; i < 10; i++) {
+        cout << "Escreva um numero: " << "loop [" << i + 1 << "]" << endl;
+        cin >> num;
+        if (i == 0) {
+            maior = num;
+            menor = num;
+        }
