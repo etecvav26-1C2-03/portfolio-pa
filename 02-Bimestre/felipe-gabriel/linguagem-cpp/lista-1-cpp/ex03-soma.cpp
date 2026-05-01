dc831af430806a249f6bba2819b17aec1f06e335
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-01 16:09:12 -0300

    Update ex03-soma.cpp

--- 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex03-soma.cpp
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex03-soma.cpp
@@ -1,1 +1,18 @@
-a
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: soma.cpp
+* Data: 01/05/2026
+* Autor: Gabriel Fernandes Barbarini
+* Descrição: 
+* Ler vários números até que seja digitado um número negativo. 
+* Calcular e exibir a soma desses números.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 0, cont = 0;
+
