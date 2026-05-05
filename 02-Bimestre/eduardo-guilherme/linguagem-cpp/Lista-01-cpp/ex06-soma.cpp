4fa6ece16f5ba704efa1906b3b15669036003cd9
Guilherme Lakonski <guilhermemiguel633@gmail.com>
2026-05-05 08:40:46 -0300

    Update ex06-soma.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-01-cpp/ex06-soma.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-01-cpp/ex06-soma.cpp
@@ -1,1 +1,14 @@
+// Exercício-6 c++
 
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	int soma = 0;
+	long long multiplicacao = 1;
+	
+	while(soma ,= 100) {
+		cout ,, "EScreva um Número:   "
+	}
+	
+}
