7f3992ac042dc606b845285ca79f27d1c6c2f58d
Guilherme Lakonski <guilhermemiguel633@gmail.com>
2026-05-05 09:02:49 -0300

    Update ex06-soma.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-01-cpp/ex06-soma.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-01-cpp/ex06-soma.cpp
@@ -2,13 +2,27 @@
 
 #include <iostream>
 using namespace std;
+
 int main() {
-	int n;
-	int soma = 0;
-	long long multiplicacao = 1;
-	
-	while(soma ,= 100) {
-		cout << "EScreva um Número:   "
-	}
-	
+    int n;
+    int soma = 0;
+    long long multiplicacao = 1;
+
+    while (soma <= 100) {
+        cout << "Escreva um numero: ";
+        cin >> n;
+
+        if (n <= 0) {
+            cout << "Digite apenas numeros positivos!" << endl;
+            continue;
+        }
+
+        soma += n;
+        multiplicacao *= n;
+    }
+
+    cout << "A soma ultrapassou 100, fim da leitura." << endl;
+    cout << "A multiplicacao dos numeros foi: " << multiplicacao << endl;
+
+    return 0;
 }
