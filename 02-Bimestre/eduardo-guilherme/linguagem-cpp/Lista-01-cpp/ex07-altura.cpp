ea4848b8a61c650d9c99b941c37f019a02355b18
Guilherme Lakonski <guilhermemiguel633@gmail.com>
2026-05-06 20:31:55 -0300

    Update ex07-altura.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-01-cpp/ex07-altura.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-01-cpp/ex07-altura.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+int main() {
+    int pessoas;
+    float altura, soma = 0, media;
+
+    std::cout << "Digite a quantidade de pessoas: ";
+    std::cin >> pessoas;
+
+    for(int i = 1; i <= pessoas; i++) {
+        std::cout << "Digite a altura da pessoa " << i << ": ";
+        std::cin >> altura;
+
+        soma += altura;
+    }
+
+    media = soma / pessoas;
+
+    std::cout << "A altura media e: " << media;
+
+    return 0;
+}
