9054a3b6c85b5d8be6f7aceae0987abcb7fa58f8
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-07 13:49:37 -0300

    Update ex08-par-impar.cpp

--- 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex08-par-impar.cpp
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-1-cpp/ex08-par-impar.cpp
@@ -1,7 +1,7 @@
 /*
 * Instituição: EtecVAV - Vasco Antonio Vechiarutti
 *
-* Arquivo: 08ex.cpp
+* Arquivo: par e impar.cpp
 * Data: 01/05/2026
 * Autor: Felipe Barbosa Santos
 * Descrição: 
@@ -13,20 +13,22 @@
 using namespace std;
 
 int main() {
-    int n, somaim = 0, somapr = 0;
+    int n, somaim = 0, somapa = 0;
     
     cout << "numero limitador: ";
     cin >> n;
-    
+
+    //calcular a soma dos pares
     for (int i = 0; i <= n; i++) {
         if (i % 2 == 0) {
-            somapr += i;
+            somapa += i;
         }
+        //soma dos impares
         else {
             somaim += i;
         }
     }
-    cout << "soma dos pares: " << somapr << endl;
+    cout << "soma dos pares: " << somapa << endl;
     cout << "soma dos impares: " << somaim;
     return 0;
 }
