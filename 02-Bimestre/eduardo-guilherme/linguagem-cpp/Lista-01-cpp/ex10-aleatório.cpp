0e0c5b3434ca0cb5f019ffc11c4f52a52a64287b
Guilherme Lakonski <guilhermemiguel633@gmail.com>
2026-05-06 21:06:21 -0300

    Update ex10-aleatório.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-01-cpp/ex10-aleatório.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-01-cpp/ex10-aleatório.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+int main() {
+
+    std::srand(std::time(0));
+
+    int numeroSecreto = std::rand() % 100 + 1;
+    int palpite;
+    int tentativas = 0;
+
+    std::cout << "Jogo de Adivinhacao!" << std::endl;
+
+    do {
+
+        std::cout << "Digite seu palpite: ";
+
+        if(!(std::cin >> palpite)) {
+            std::cout << "Erro ao ler o numero!";
+            return 0;
+        }
+
+        tentativas++;
+
+        if(palpite > numeroSecreto) {
+            std::cout << "Mais!" << std::endl;
+        }
+        else if(palpite < numeroSecreto) {
+            std::cout << "Menos!" << std::endl;
+        }
+        else {
+            std::cout << "Correto!" << std::endl;
+        }
+
+    } while(palpite != numeroSecreto);
+
+    std::cout << "Quantidade de tentativas: " << tentativas;
+
+    return 0;
+}
