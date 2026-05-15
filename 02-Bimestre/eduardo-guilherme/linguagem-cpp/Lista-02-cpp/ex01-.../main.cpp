ff398b4d6066f6760d139d5425817aa3c18167f9
Eduardo Bargueiras <youndercg@gmail.com>
2026-05-15 09:45:37 -0300

    Update main.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex01-.../main.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex01-.../main.cpp
@@ -1,1 +1,29 @@
-.
+#include <iostream>
+
+using namespace std;
+
+bool impar(int n){
+	return n % 2 == 1;
+}
+
+int main(int argc, char** argv) {
+	int n1, vfinal;
+	
+	cout << "Escreva um numero:";
+	cin >> n1;
+	
+	for(int i = 0;i <= n1;i++){
+		if(impar(i)){
+			vfinal += i;
+			if(i + 2 <= n1){
+				cout << i << "+";
+			}else{
+				cout << i;
+			}
+		}
+	}
+	
+	cout << "=" << vfinal;
+	
+	return 0;
+}
