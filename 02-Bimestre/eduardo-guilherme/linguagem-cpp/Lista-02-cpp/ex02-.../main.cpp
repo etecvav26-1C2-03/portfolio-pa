be16187ad3070493e59e8ed33784145462d7d7f7
Eduardo Bargueiras <youndercg@gmail.com>
2026-05-15 10:51:19 -0300

    Update main.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex02-.../main.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex02-.../main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char** argv) {
+	int b, e, r;
+	
+	cout << "Digite o valor da base:";
+	cin >> b;
+	
+	cout << "Agora insira o valor do expoente:";
+	cin >> e;
+	
+	for (int i = 1;i == e;i++){
+		r *= b;
+		cout << r << endl;
+	}
+	
+	return 0;
+}
