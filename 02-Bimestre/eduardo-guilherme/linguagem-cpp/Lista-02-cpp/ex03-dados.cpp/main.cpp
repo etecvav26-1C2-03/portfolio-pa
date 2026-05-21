e3b3101675fca7ae4626879d61d23d1281572b72
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-21 14:41:38 -0300

    Create main.cpp

---
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex03-dados.cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	int altura;
+	
+	std::cout << "Escreva sua altura(ex:132 para 1 metro e 32 centimetros):" << std::endl << std::endl << ">> ";
+	std::cin >> altura;
+	
+    return 0;
+}
