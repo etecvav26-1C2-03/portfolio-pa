a91d1f4902709ee98e99eb571cbff8adc5111f6f
Eduardo Bargueiras <youndercg@gmail.com>
2026-05-27 11:14:01 -0300

    Update main.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex04-calculadora.cpp/main.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex04-calculadora.cpp/main.cpp
@@ -12,11 +12,21 @@ int subtracao(int n1, int n2){
 	return conta;
 }
 
+int multiplicacao(int n1, int n2){
+	int conta = n1 * n2;
+	
+	return conta;
+}
 
-
+float divisao(int n1, int n2){
+	float conta = (float)n1 / n2;
+	
+	return conta;
+}
 
 int main(int argc, char** argv) {
-	int n1, n2, resultado_soma, resultado_subtracao, resultado_mutiplicacao, resultado_divisao;
+	int n1, n2, resultado_soma, resultado_subtracao, resultado_multiplicacao;
+	float resultado_divisao;
 	
 	std::cout << "Insira dois numeros de valor INTEIRO para a calculadora:\n\nN1 >> ";
 	std::cin >> n1;
@@ -26,12 +36,18 @@ int main(int argc, char** argv) {
 	
 	resultado_soma = soma(n1, n2);
 	resultado_subtracao = subtracao(n1, n2);
+	resultado_multiplicacao = multiplicacao(n1, n2);
+	resultado_divisao = divisao(n1, n2);
 	
 	std::cout << "\n------ RESULTADOS ------\n\n" << "Soma:\n>> ";
 	
 	std::cout << n1 << " + " << n2 << " = " << resultado_soma << "\n\nSubtracao:\n>> ";
-	std::cout << n1 << " - " << n2 << " = " << resultado_subtracao;
+	std::cout << n1 << " - " << n2 << " = " << resultado_subtracao << "\n\nMultiplicacao:\n>> ";
+	
+	std::cout << n1 << " * " << n2 << " = " << resultado_multiplicacao << "\n\nDivisao:\n>> ";
+	std::cout << n1 << " / " << n2 << " = " << resultado_divisao << "\n\n------------------------";
 
 	return 0;
 }
+
 
