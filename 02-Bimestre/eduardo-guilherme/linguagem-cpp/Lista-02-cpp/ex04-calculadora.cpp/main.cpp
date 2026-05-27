4c3fddcc146da1a923ab88f117bcd994fb75766e
Eduardo Bargueiras <youndercg@gmail.com>
2026-05-27 10:54:02 -0300

    Update main.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex04-calculadora.cpp/main.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex04-calculadora.cpp/main.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+
+int soma(int n1, int n2){
+	int conta = n1 + n2;
+	
+	return conta;
+}
+
+int subtracao(int n1, int n2){
+	int conta = n1 - n2;
+	
+	return conta;
+}
+
+
+
+
+int main(int argc, char** argv) {
+	int n1, n2, resultado_soma, resultado_subtracao, resultado_mutiplicacao, resultado_divisao;
+	
+	std::cout << "Insira dois numeros de valor INTEIRO para a calculadora:\n\nN1 >> ";
+	std::cin >> n1;
+	
+	std::cout << "\nN2 >> ";
+	std::cin >> n2;
+	
+	resultado_soma = soma(n1, n2);
+	resultado_subtracao = subtracao(n1, n2);
+	
+	std::cout << "\n------ RESULTADOS ------\n\n" << "Soma:\n>> ";
+	
+	std::cout << n1 << " + " << n2 << " = " << resultado_soma << "\n\nSubtracao:\n>> ";
+	std::cout << n1 << " - " << n2 << " = " << resultado_subtracao;
+
+	return 0;
+}
 
