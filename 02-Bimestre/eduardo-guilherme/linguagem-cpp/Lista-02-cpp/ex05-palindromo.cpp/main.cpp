334431b9352d0ba9e6057e0960d651297d92f0ef
Eduardo Bargueiras <youndercg@gmail.com>
2026-05-27 11:25:47 -0300

    Update main.cpp

--- 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex05-palindromo.cpp/main.cpp
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex05-palindromo.cpp/main.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <string>
 
+int main(int argc, char** argv) {
+	std::string palavra;
+	
+	std::cout << "Escreva uma palavra para verificarmos ela:\n\n>> ";
+	std::cin >> palavra;
+
+
+	return 0;
+}
