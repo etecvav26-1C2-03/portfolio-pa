0188809d8344cf4166c3a2e770a59d8ec33faad0
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-31 13:43:09 -0300

    Update main.cpp

---
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-2-cpp/ex02-potencia.cpp/main.cpp
@@ -0,0 +1,32 @@
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: ex02.cpp
+* Data: 30/05/2026
+* Autor: Gabriel Fernandes Barbarini
+* Descrição:
+* Ler a base e o expoente. Calcular a potência usando repetição.
+* Criar uma função para o cálculo.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int pon() {
+    int B, E, multi = 1;
+    cin >> B;
+    cin >> E;
+    for (int i=0; i<E; i++) {
+        multi*=B;
+    }
+    cout << multi;
+    return 0;
+}
+
+int main() {
+    pon();
+    return 0;
+}
