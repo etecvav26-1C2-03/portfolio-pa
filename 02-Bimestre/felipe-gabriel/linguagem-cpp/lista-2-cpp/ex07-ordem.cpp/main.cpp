eec46576c0046bd9da1e02003f62630356d5c263
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-31 19:49:23 -0300

    Create main.cpp

---
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-2-cpp/ex07-ordem.cpp/main.cpp
@@ -0,0 +1,35 @@
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: ex07.cpp
+* Data: 31/05/226
+* Autor: Gabriel Fernandes Barbarini
+* Descrição:
+* Ler 5 números. Verificar se estão em ordem crescente,
+* em ordem decrescente ou não ordenados.
+* Criar uma função para essa análise.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void order() {
+    vector<int> n(5);
+    bool c = true, d = true;
+    for (int i = 0; i < 5; i++) cin >> n[i];
+    for (int i = 0; i < 4; i++) {
+        if (n[i] > n[i+1]) c = false;
+        else d = false;
+    }
+    if (c) cout << "crescente";
+    else if (d) cout << "descrescente";
+    else cout << "nao ordenado";
+}
+
+int main() {
+    order();
+    return 0;
+}
