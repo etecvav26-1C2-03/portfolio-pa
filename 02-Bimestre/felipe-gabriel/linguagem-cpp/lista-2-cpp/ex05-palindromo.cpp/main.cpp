b26b94f8de3f1a5e381513959da5d1d039381c2d
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-31 13:50:59 -0300

    Create main.cpp

---
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-2-cpp/ex05-palindromo.cpp/main.cpp
@@ -0,0 +1,34 @@
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: ex05.cpp
+* Data: 30/05/2026
+* Autor: Gabriel Fernandes Barbarini
+* Descrição:
+* Ler uma palavra. Verificar se é palíndromo.
+* Criar uma função que retorne verdadeiro ou falso.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void palin() {
+    string word;
+    cin >> word;
+    string word2 = word;
+    reverse(word2.begin(), word2.end());
+    if (word == word2) {
+        cout << "is palindromo";
+    }
+    else {
+        cout << "not is palindromo";
+    }
+}
+
+int main() {
+    palin();
+    return 0;
+}
