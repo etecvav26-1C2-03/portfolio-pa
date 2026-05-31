5422087fbc5154511e5150759435bb130bd0919c
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-31 13:41:38 -0300

    Update main.cpp

--- 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-2-cpp/ex01-impar.cpp/main.cpp
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-2-cpp/ex01-impar.cpp/main.cpp
@@ -1,36 +1,28 @@
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: ex01.cpp
+* Data: 30/05/2026
+* Autor: Gabriel Fernandes Barbarini
+* Descrição:
+* Ler um número inteiro. Exibir a soma dos números ímpares até o número lido.
+* Criar uma função que retorne verdadeiro para números ímpares.
+*/
+
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-bool impar(int numero)
-{
-    return numero % 2 == 1;
-}
-
-int main()
-{
-    int numero;
-    int soma = 0;
-
-    cout << "Digite um numero: ";
-    cin >> numero;
-
-    for(int i = 1; i <= numero; i++)
-    {
-        if(impar(i))
-        {
-            soma += i;
-
-            cout << i;
-
-            
-            if(i + 2 <= numero)
-            {
-                cout << " + ";
-            }
+bool impar() {
+    int N, sum = 0;
+    bool found = false;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+            found = true;
         }
     }
-
-    cout << " = " << soma << endl;
-
-    return 0;
-}
+    
