dfbfc24cfb6dbf15de6145238d48766285a2f79b
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-31 19:53:56 -0300

    Update main.cpp

--- 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-2-cpp/ex09-vogais.cpp/main.cpp
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-2-cpp/ex09-vogais.cpp/main.cpp
@@ -18,7 +18,14 @@ using namespace std;
 void substring() {
     string w;
     cin >> w;
-    
+    for (char& c : w) {
+        if (c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u') {
+            c = '*';
+        }
+    }
+    cout << w;
+}
+
 int main() {
     substring();
     return 0;
