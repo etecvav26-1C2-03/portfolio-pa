d1a17a9462daab54ea60dc028ad0278db9513acd
Gabriel Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-05-31 19:53:34 -0300

    Create main.cpp

---
+++ 02-Bimestre/felipe-gabriel/linguagem-cpp/lista-2-cpp/ex09-vogais.cpp/main.cpp
@@ -0,0 +1,25 @@
+/*
+* Instituição: EtecVAV - Vasco Antonio Vechiarutti
+*
+* Arquivo: ex09.cpp
+* Data: 31/05/2026 
+* Autor: Gabriel Fernandes Barbarini
+* Descrição:
+* Ler uma palavra. Substituir todas as vogais por '*'.
+* Criar uma função para realizar a substituição.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void substring() {
+    string w;
+    cin >> w;
+    
+int main() {
+    substring();
+    return 0;
+}
