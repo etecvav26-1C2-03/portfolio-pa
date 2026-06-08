4b9f158f8b4b724af57d8db1c91fc187dfa33940
Eduardo Bargueiras <youndercg@gmail.com>
2026-06-08 08:00:13 -0300

    Create main.cpp

---
+++ 02-Bimestre/eduardo-guilherme/linguagem-cpp/Lista-02-cpp/ex08-area.cpp/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+double area(double raio){
+    return (raio * raio) * 3.14;
+}
+
+int main() {
+    double r1, r2, r3, r4, r5, areaf;
+
+    std::cout << "Digite o raio de 5 circulos (em centimetros/cm):\n\nR1>> ";
+    std::cin >> r1;
+
+    std::cout << "R2>> ";
+    std::cin >> r2;
+
+    std::cout << "R3>> ";
+    std::cin >> r3;
+
+    std::cout << "R4>> ";
+    std::cin >> r4;
+
+    std::cout << "R5>> ";
+    std::cin >> r5;
+
+    areaf = area(r1) + area(r2) + area(r3) + area(r4) + area(r5);
+
+    std::cout << "\nSoma das areas dos 5 circulos:\n\n>> " << areaf;
+
+    return 0;
+}
