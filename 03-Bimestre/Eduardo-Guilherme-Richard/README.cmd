5ed03458e10a883eb25f5a91d8ceb2e42d2348c3
GUILHERME MIGUEL RODRIGUES PEREIRA LAKONSKI <guilherme.miguelrex1@gmail.com>
2026-08-06 13:54:53 -0300

    Update README.cmd

--- 03-Bimestre/Eduardo-Guilherme-Richard/README.cmd
+++ 03-Bimestre/Eduardo-Guilherme-Richard/README.cmd
@@ -2,6 +2,3 @@
 -Guilherme Miguel Rodrigues Pereira Lakonski
 -Eduardo Antônio de Oliveira Bargueiras
 -Richard Murilo Araújo Freire
-<p align="center">
- <img src="images/furry.png" width="300">
-</p>
