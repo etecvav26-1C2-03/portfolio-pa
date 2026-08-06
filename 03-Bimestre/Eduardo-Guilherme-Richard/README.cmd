59bd5cfe8085bac964c59fb74222721dd4d2c598
GUILHERME MIGUEL RODRIGUES PEREIRA LAKONSKI <guilherme.miguelrex1@gmail.com>
2026-08-06 13:49:13 -0300

    Update README.cmd

--- 03-Bimestre/Eduardo-Guilherme-Richard/README.cmd
+++ 03-Bimestre/Eduardo-Guilherme-Richard/README.cmd
@@ -3,5 +3,5 @@
 -Eduardo Antônio de Oliveira Bargueiras
 -Richard Murilo Araújo Freire
 <p align="center">
-  <img src="https://www.reddit.com/r/furry/comments/c02whv/im_straight_please_dont_kill_me_by_kiro_fennec/?tl=pt-br" width="250">
+  <img src="https://preview.redd.it/im-straight-please-dont-kill-me-by-kiro-fennec-v0-xk9yyelrv2431.jpg?width=1080&crop=smart&auto=webp&s=9755c7178c9cbfdfbd353f3b8c707b9672efa78b" width="250">
 </p>
