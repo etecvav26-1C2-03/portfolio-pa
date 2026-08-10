5d0ef2e3c536dfd65053711e5e1f1ef3f488d526
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-10 14:38:32 -0300

    Update ex01.py

--- 03-Bimestre/cauan-felipe-gabriel/Gabriel/lista-01/ex01/ex01.py
+++ 03-Bimestre/cauan-felipe-gabriel/Gabriel/lista-01/ex01/ex01.py
@@ -1,1 +1,21 @@
-a
+''' 
+Instituição: EtecVAV - Vasco Antonio Vechiarutti
+
+Arquivo: numero.py
+Data: 10/08/2026
+Autor: Gabriel Fernandes Barbarini
+Descrição: 
+Ler um número, calcular e exibir se esse número é:
+positivo, negativo ou zero.
+'''
+
+num = int(input("Digite um número: "))
+
+if num > 0:
+    print("Seu número é positivo")
+
+elif num == 0:
+    print ("Seu número é 0")
+
+else:
+    print("Seu número é negativo")
