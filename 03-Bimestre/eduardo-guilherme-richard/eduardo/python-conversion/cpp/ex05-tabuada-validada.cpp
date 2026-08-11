d10d22c6cf4a00aa433d6b80f198248fc4269643
Eduardo Bargueiras <youndercg@gmail.com>
2026-08-11 13:44:02 -0300

    Update ex05-tabuada-validada.cpp

--- 03-Bimestre/eduardo-guilherme-richard/eduardo/python-conversion/cpp/ex05-tabuada-validada.cpp
+++ 03-Bimestre/eduardo-guilherme-richard/eduardo/python-conversion/cpp/ex05-tabuada-validada.cpp
@@ -1,6 +1,6 @@
 /*
     Autor: Eduardo Antônio de Oliveira Bargueiras
-    Data: Junho/2026
+    Data: 11/08/2026
     Descrição: Lê um número de 1 a 10 e exibe sua tabuada, validando a entrada.
 */
 
