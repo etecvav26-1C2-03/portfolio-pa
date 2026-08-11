4c26b339d198a48c6c5bc65f3abba3713c3f4098
Eduardo Bargueiras <youndercg@gmail.com>
2026-08-11 06:25:05 -0300

    rework in code for better otimization

--- EXTRA/Eduardo/jogo-rpg/main.cpp
+++ EXTRA/Eduardo/jogo-rpg/main.cpp
@@ -2,8 +2,6 @@
 #include <random>
 #include <ctime>
 
-// ATENCAO: Este codigo exige C++11 ou superior. Use programas como Programiz para testar o código.
-
 std::mt19937 gen(time(0));
 
 int GarraJerimum(){
@@ -35,129 +33,170 @@ void batalhaF(){
     int escolha;
     int hpFelipe = 100;
     int hpInimigo = 150;
-    
-    std::cout << " (Loading...)\n====================================================\n\nFORMIGUINHA_CURSED apareceu!\n(Seu HP: " << hpFelipe << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
-    std::cin >> escolha;
-    
-    if(escolha == 1){
-        int danoFelipe = DanoFelipe();
-        hpInimigo -= danoFelipe;
-        std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoFelipe << " de dano.";
+    int turno = 1;
+    int buff = 0;
+
+    std::cout << " (Loading...)\n====================================================\n\nFORMIGUINHA_CURSED apareceu!\n";
+
+    while(hpFelipe > 0 && hpInimigo > 0){
+        std::cout << "\n\n========== TURNO " << turno << ": ==========\n\n(Seu HP: " << hpFelipe << ")\n(HP da FORMIGUINHA_CURSED: " << hpInimigo << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
+        std::cin >> escolha;
+
+        if(escolha == 1){
+            int danoFelipe = DanoFelipe();
+            hpInimigo -= danoFelipe;
+            std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoFelipe << " de dano.";
+        }
+        else if(escolha == 2){
+            std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
+        }
+
+        if(hpInimigo <= 0){
+            break;
+        }
+
+        if(turno % 3 == 0){
+            std::cout << "\n\nFORMIGUINHA_CURSED canta uma melodia assustadora... (buff: +15 de dano)";
+            buff += 15;
+        }
+        else if(turno % 5 == 0){
+            int danoGarraJerimum = GarraJerimum() + buff;
+            hpFelipe -= danoGarraJerimum;
+            std::cout << "\n\nFORMIGUINHA_CURSED usou GARRA DO JERIMUM e causou " << danoGarraJerimum << " de dano.";
+        }
+        else{
+            int danoInimigo = DanoInimigo() + buff;
+            hpFelipe -= danoInimigo;
+            std::cout << "\n\nFORMIGUINHA_CURSED atacou e causou " << danoInimigo << " de dano.";
+        }
+
+        turno++;
     }
-    else if(escolha == 2){
-        std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
-    }
-    std::cout << "\n\nFORMIGUINHA_CURSED canta uma melodia assustadora... (buff: +15 de dano)";
-    
-    std::cout << "\n\n========== TURNO 2: ==========\n\n(Seu HP: " << hpFelipe << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
-    std::cin >> escolha;
-    
-    if(escolha == 1){
-        int danoFelipe = DanoFelipe();
-        hpInimigo -= danoFelipe;
-        std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoFelipe << " de dano.\n\n";
-    }
-    else if(escolha == 2){
-        std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
-    }
-    
-    int danoGarraJerimum = GarraJerimum() + 15;
-    hpFelipe -= danoGarraJerimum;
-    
-    std::cout << "FORMIGUINHA_CURSED usou GARRA DO JERIMUM e causou " << danoGarraJerimum << " de dano.";
-    
+
     system("cls");
-    std::cout << "========== TURNO 3: ==========\n\n(Seu HP: " << hpFelipe << ")";
+
+    if(hpInimigo <= 0){
+        std::cout << "\nVoce derrotou a FORMIGUINHA_CURSED! Voce venceu!";
+    }
+    else{
+        std::cout << "\nVoce foi derrotado pela FORMIGUINHA_CURSED...";
+    }
 }
 
 void batalhaB(){
     int escolha;
     int hpBarbarini = 200;
     int hpInimigo = 150;
-    
-    std::cout << " (Loading...)\n====================================================\n\nFORMIGUINHA_CURSED apareceu!\n(Seu HP: " << hpBarbarini << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
-    std::cin >> escolha;
-    
-    if(escolha == 1){
-        int danoBarbarini = DanoBarbarini();
-        hpInimigo -= danoBarbarini;
-        
-        std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoBarbarini << " de dano.";
+    int turno = 1;
+    int buff = 0;
+
+    std::cout << " (Loading...)\n====================================================\n\nFORMIGUINHA_CURSED apareceu!\n";
+
+    while(hpBarbarini > 0 && hpInimigo > 0){
+        std::cout << "\n\n========== TURNO " << turno << ": ==========\n\n(Seu HP: " << hpBarbarini << ")\n(HP da FORMIGUINHA_CURSED: " << hpInimigo << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
+        std::cin >> escolha;
+
+        if(escolha == 1){
+            int danoBarbarini = DanoBarbarini();
+            hpInimigo -= danoBarbarini;
+            std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoBarbarini << " de dano.";
+        }
+        else if(escolha == 2){
+            std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
+        }
+
+        if(hpInimigo <= 0){
+            break;
+        }
+
+        if(turno % 3 == 0){
+            std::cout << "\n\nFORMIGUINHA_CURSED canta uma melodia assustadora... (buff: +15 de dano)";
+            buff += 15;
+        }
+        else if(turno % 5 == 0){
+            int danoGarraJerimum = GarraJerimum() + buff;
+            hpBarbarini -= danoGarraJerimum;
+            std::cout << "\n\nFORMIGUINHA_CURSED usou GARRA DO JERIMUM e causou " << danoGarraJerimum << " de dano.";
+        }
+        else{
+            int danoInimigo = DanoInimigo() + buff;
+            hpBarbarini -= danoInimigo;
+            std::cout << "\n\nFORMIGUINHA_CURSED atacou e causou " << danoInimigo << " de dano.";
+        }
+
+        turno++;
     }
-    else if(escolha == 2){
-        std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
-    }
-    std::cout << "\nFORMIGUINHA_CURSED canta uma melodia assustadora... (buff: +15 de dano)";
-    
-    std::cout << "\n\n========== TURNO 2: ==========\n\n(Seu HP: " << hpBarbarini << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
-    std::cin >> escolha;
-    
-    if(escolha == 1){
-        int danoBarbarini = DanoBarbarini();
-        hpInimigo -= danoBarbarini;
-        
-        std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoBarbarini << " de dano.";
-    }
-    else if(escolha == 2){
-        std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
-    }
-    
-    int danoGarraJerimum = GarraJerimum() + 15;
-    hpBarbarini -= danoGarraJerimum;
-    
-    std::cout << "\nFORMIGUINHA_CURSED usou GARRA DO JERIMUM e causou " << danoGarraJerimum << " de dano.";
-    
+
     system("cls");
-    std::cout << "========== TURNO 3: ==========\n\n(Seu HP: " << hpBarbarini << ")";
+
+    if(hpInimigo <= 0){
+        std::cout << "\nVoce derrotou a FORMIGUINHA_CURSED! Voce venceu!";
+    }
+    else{
+        std::cout << "\nVoce foi derrotado pela FORMIGUINHA_CURSED...";
+    }
 }
 
 void batalhaK(){
     int escolha;
     int hpKevin = 50;
     int hpInimigo = 150;
-    
-    std::cout << " (Loading...)\n====================================================\n\nFORMIGUINHA_CURSED apareceu!\n(Seu HP: " << hpKevin << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
-    std::cin >> escolha;
-    
-    if(escolha == 1){
-        int danoKevin = DanoKevin();
-        hpInimigo -= danoKevin;
-        
-        std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoKevin << " de dano.";
+    int turno = 1;
+    int buff = 0;
+
+    std::cout << " (Loading...)\n====================================================\n\nFORMIGUINHA_CURSED apareceu!\n";
+
+    while(hpKevin > 0 && hpInimigo > 0){
+        std::cout << "\n\n========== TURNO " << turno << ": ==========\n\n(Seu HP: " << hpKevin << ")\n(HP da FORMIGUINHA_CURSED: " << hpInimigo << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
+        std::cin >> escolha;
+
+        if(escolha == 1){
+            int danoKevin = DanoKevin();
+            hpInimigo -= danoKevin;
+            std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoKevin << " de dano.";
+        }
+        else if(escolha == 2){
+            std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
+        }
+
+        if(hpInimigo <= 0){
+            break;
+        }
+
+        if(turno % 3 == 0){
+            std::cout << "\n\nFORMIGUINHA_CURSED canta uma melodia assustadora... (buff: +15 de dano)";
+            buff += 15;
+        }
+        else if(turno % 5 == 0){
+            int danoGarraJerimum = GarraJerimum() + buff;
+            hpKevin -= danoGarraJerimum;
+            std::cout << "\n\nFORMIGUINHA_CURSED usou GARRA DO JERIMUM e causou " << danoGarraJerimum << " de dano.";
+        }
+        else{
+            int danoInimigo = DanoInimigo() + buff;
+            hpKevin -= danoInimigo;
+            std::cout << "\n\nFORMIGUINHA_CURSED atacou e causou " << danoInimigo << " de dano.";
+        }
+
+        turno++;
     }
-    else if(escolha == 2){
-        std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
-    }
-    std::cout << "\nFORMIGUINHA_CURSED canta uma melodia assustadora... (buff: +15 de dano)";
-    
-    std::cout << "\n\n========== TURNO 2: ==========\n\n(Seu HP: " << hpKevin << ")\n\n1 - ATACAR     2 - POUPAR\n\n>>> ";
-    std::cin >> escolha;
-    
-    if(escolha == 1){
-        int danoKevin = DanoKevin();
-        hpInimigo -= danoKevin;
-        
-        std::cout << "\nVoce atacou a FORMIGUINHA_CURSED e causou " << danoKevin << " de dano.";
-    }
-    else if(escolha == 2){
-        std::cout << "\nFORMIGUINHA_CURSED foi poupada...";
-    }
-    
-    int danoGarraJerimum = GarraJerimum() + 15;
-    hpKevin -= danoGarraJerimum;
-    
-    std::cout << "\nFORMIGUINHA_CURSED usou GARRA DO JERIMUM e causou " << danoGarraJerimum << " de dano.";
-    
+
     system("cls");
-    std::cout << "========== TURNO 3: ==========\n\n(Seu HP: " << hpKevin << ")";
+
+    if(hpInimigo <= 0){
+        std::cout << "\nVoce derrotou a FORMIGUINHA_CURSED! Voce venceu!";
+    }
+    else{
+        std::cout << "\nVoce foi derrotado pela FORMIGUINHA_CURSED...";
+    }
 }
 
 int main(int argc, char** argv) {
     int escolha;
-    
+
     std::cout << "Escolha o avatar(ISSO AFETA OS STATUS):\n\n\n1 - FELIPE     2 - BARBARINI     3 - KEVIN\n\n(ESCREVA DO 1 AO 3)\n\n>>> ";
     std::cin >> escolha;
-    
+
     if(escolha == 1){
         std::cout << "\nEscolheu o FELIPE!";
         batalhaF();
@@ -172,7 +211,7 @@ int main(int argc, char** argv) {
     }
     else{
         std::cout << "\nTente novamente...";
-    }    
-    
+    }
+
     return 0;
 }
