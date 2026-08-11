1e0630a0b7239dd4dc677c31c6902f985b1d9a36
Felipe Santos <felipessantosbsa@gmail.com>
2026-08-11 14:22:46 -0300

    feat: add ex01 python

---
+++ 03-Bimestre/cauan-felipe-gabriel/felipe/python-conversion/python/ex01/main.py
@@ -0,0 +1,10 @@
+while True:
+    n = int(input())
+    if n > 0:
+        print(f"> {n} = maior que zero")
+        break
+    elif n == 0:
+        print(f"> {n} = igual a zero")
+    else:
+        print(f"> {n} = menor que zero")
+        break
