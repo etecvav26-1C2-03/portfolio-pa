15dbde10da38360bb691a00d86bb8a8a4408fd2d
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 13:37:21 -0300

    Add author, date, and description to script

--- 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex02-maior-numero.py
+++ 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex02-maior-numero.py
@@ -1,1 +1,14 @@
-a
+'''
+    Autor: Gabriel Fernandes Barbarini
+    Data: 10/08/2026
+    Descrição: Lê dois números inteiros e informa qual é o maior ou se são iguais.
+'''
+
+numero1 = int (input("Escreva o primeiro numero: "))
+numero2 = int (input("Escreva o segundo numero: "))
+if numero1 > numero2:
+    print ("O numero ", numero1 ,"é maior que o numero ", numero2)
+elif numero1 < numero2:
+    print ("O numero " , numero1 , "é menor que o numero " , numero2)
+else:
+    print("Os numeros tem o mesmo valor")
