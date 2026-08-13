aa642465f20f6548ac0c82e9ae0c7cab42cf0f91
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 14:25:55 -0300

    Update ex07-a.py

--- 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/python/ex07-a.py
+++ 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/python/ex07-a.py
@@ -4,4 +4,21 @@
     Descrição: Lê várias notas e calcula e média entre elas.
 '''
 
-aa
+contagem = 1
+positivo = 0
+negativo = 0
+zero = 0
+
+while (contagem <= 10):
+    numero = int(input("Digite um numero: "))
+
+    if (numero > 0):
+        positivo = positivo + 1
+
+    elif (numero < 0):
+        negativo = negativo + 1
+
+    else:
+        zero = zero + 1
+
+    contagem = contagem + 1
