8d76ad8a6a1ab977b2ebc2b26306bd50f8ade338
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 14:48:59 -0300

    Create main.cpp

---
+++ 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/cpp/ex04-soma-par/main.cpp
@@ -0,0 +1,34 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero;
+    int contador = 1;
+    int soma = 0;
+
+    cout << "Digite um numero inteiro positivo: ";
+    cin >> numero;
+
+    if (numero > 0)
+    {
+        while (contador <= numero)
+        {
+            if (contador % 2 == 0)
+            {
+                soma = soma + contador;
+            }
+
+            contador++;
+        }
+
+        cout << "Soma dos pares: " << soma << endl;
+    }
+    else
+    {
+        cout << "Numero invalido." << endl;
+    }
+
+    return 0;
+}
