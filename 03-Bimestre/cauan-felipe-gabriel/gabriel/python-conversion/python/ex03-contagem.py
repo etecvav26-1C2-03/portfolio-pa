c7e6f7f6221529624b8f66e52eb6fa26d5a552d6
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 13:47:16 -0300

    Update ex03-contagem.py

--- 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex03-contagem.py
+++ 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex03-contagem.py
@@ -1,1 +1,11 @@
-a
+'''
+    Autor: Gabriel Fernandes Barbarini
+    Data: 10/08/2026
+    Descrição: Lê um número inteiro positivo e exibe a contagem de 1 até esse número.
+'''
+contagem = 1
+numero = int (input("Escreva seu numero: "))
+if numero >= 0:
+    while contagem <= numero: 
+        print(contagem)
+        contagem +=
