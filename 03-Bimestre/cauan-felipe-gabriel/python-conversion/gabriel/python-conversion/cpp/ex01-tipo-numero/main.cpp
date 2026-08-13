dfb53ff1460215bd15257825c277bed48394efaa
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 14:46:34 -0300

    Create main.cpp

---
+++ 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/cpp/ex01-tipo-numero/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero;
+
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+
+    if (numero > 0)
+    {
+        cout << "O numero e positivo." << endl;
+    }
+    else if (numero < 0)
+    {
+        cout << "O numero e negativo." << endl;
+    }
+    else
+    {
+        cout << "O numero e zero." << endl;
+    }
+
+    return 0;
+}
