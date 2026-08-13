d81afe58fc5e1cda2c782182cf52f7b6d5631c27
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 14:20:42 -0300

    Add average calculation for valid grades input

--- 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/python/ex06-media-nota.py
+++ 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/python/ex06-media-nota.py
@@ -9,3 +9,8 @@ while (nota >= 0 and nota <= 10):
 
     nota = float(input("Digite uma nota entre 0 e 10: "))
 
+if (quantidade > 0):
+    print(f"Média das notas: {soma / quantidade}")
+
+else:
+    print("Nenhuma nota valida foi digitada.")
