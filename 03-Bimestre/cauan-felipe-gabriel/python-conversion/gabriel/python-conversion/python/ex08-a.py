e84f6ed4a9a8c2db8dbe005d294fb98e14ad7f31
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 14:32:48 -0300

    Update ex08-a.py

--- 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/python/ex08-a.py
+++ 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/python/ex08-a.py
@@ -3,3 +3,15 @@
     Data: 13/08/2026
     Descrição: Lê várias notas e calcula e média entre elas.
 '''
+
+numero = int(input("Digite um número: "))
+divisao = 1
+num_div = 0
+
+if (numero > 0):
+
+    while (divisao <= numero):
+        if (numero % divisao == 0):
+            num_div = num_div + 1
+
+        divisao = divisao + 1
