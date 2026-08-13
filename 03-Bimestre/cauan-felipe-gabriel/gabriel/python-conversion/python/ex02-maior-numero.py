0589e3cc125a3fc427a18f48a3d73575f8ff5193
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 13:41:09 -0300

    Update ex02-maior-numero.py

--- 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex02-maior-numero.py
+++ 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex02-maior-numero.py
@@ -6,3 +6,10 @@
 
 numero1 = int (input("Escreva o primeiro numero: "))
 numero2 = int (input("Escreva o segundo numero: "))
+
+if numero1 > numero2:
+    print ("O numero ", numero1 ,"é maior que o numero ", numero2)
+elif numero1 < numero2:
+    print ("O numero " , numero1 , "é menor que o numero " , numero2)
+else:
+    print("Os numeros tem o mesmo valor")
