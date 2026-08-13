55073d4235d9d8cb889fca9ca53665f807104e4e
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 13:29:55 -0300

    Update ex01-tipo-numero.py

--- 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex01-tipo-numero.py
+++ 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex01-tipo-numero.py
@@ -1,1 +1,13 @@
-a
+'''
+    Autor: Gabriel Fernandes Barbarini
+    Data: 10/08/2026
+    Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
+'''
+
+numero = int(input("Escreva um número: "))
+if numero > 0:
+    print("seu número é positivo")
+elif numero == 0:
+    print ("seu número é 0")
+else:
+    print("seu número é negativo")
