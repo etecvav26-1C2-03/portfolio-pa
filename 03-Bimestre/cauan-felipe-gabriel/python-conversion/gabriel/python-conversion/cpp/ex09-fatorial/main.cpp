de1398bc972d17850db5b0c4a59a56ea6ae79a18
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 14:57:13 -0300

    Create main.cpp

---
+++ 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/cpp/ex09-fatorial/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero;
+    int contador = 1;
+    int fatorial = 1;
+
+    cout << "Digite um numero inteiro positivo: ";
+    cin >> numero;
+
+    if (numero >= 0)
+    {
+        while (contador <= numero)
+        {
+            fatorial = fatorial * contador;
+            contador++;
+        }
+
+        cout << "Fatorial: " << fatorial << endl;
+    }
+    else
+    {
+        cout << "Numero invalido." << endl;
+    }
+
+    return 0;
+}
