035245c98bc93a62a2559b408ccb197c975afdd8
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 13:50:18 -0300

    Update ex04-soma-par.py

--- 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex04-soma-par.py
+++ 03-Bimestre/cauan-felipe-gabriel/gabriel/python-conversion/python/ex04-soma-par.py
@@ -1,1 +1,15 @@
-a
+'''
+    Autor: Gabriel Fernandes Barbarini
+    Data: 11/08/2026
+    Descrição: Calcula a soma dos números pares de 1 até um número informado.
+'''
+
+numero = int(input("Insira um número positivo: "))
+i = 1
+soma = 0
+if numero >= 0:
+    while i < numero:
+        i += 1
+        if i % 2 == 0:
+            soma += i   
+    print("A soma dos números pares é: ", soma) 
