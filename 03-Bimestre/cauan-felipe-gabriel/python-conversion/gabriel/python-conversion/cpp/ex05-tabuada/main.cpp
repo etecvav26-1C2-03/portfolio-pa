3236bc7a4c1a50c5c1304db43fb774aaed332046
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-13 14:50:04 -0300

    Create main.cpp

---
+++ 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/cpp/ex05-tabuada/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero;
+    int contador = 1;
+
+    cout << "Digite um numero de 1 a 10: ";
+    cin >> numero;
+
+    while (numero < 1 || numero > 10)
+    {
+        cout << "Valor invalido. Digite novamente: ";
+        cin >> numero;
+    }
+
+    while (contador <= 10)
+    {
+        cout << numero << " x " << contador << " = " << numero * contador << endl;
+        contador++;
+    }
+
+    return 0;
+}
