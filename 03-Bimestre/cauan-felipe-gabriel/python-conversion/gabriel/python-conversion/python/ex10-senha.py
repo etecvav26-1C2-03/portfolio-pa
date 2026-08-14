fa13e4dff787f50166c9d9e5a126287be99b4f0f
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-08-14 20:03:34 -0300

    Update ex10-senha.py

--- 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/python/ex10-senha.py
+++ 03-Bimestre/cauan-felipe-gabriel/python-conversion/gabriel/python-conversion/python/ex10-senha.py
@@ -1,7 +1,21 @@
 '''
     Autor: Gabriel Fernandes Barbarini
     Data: 10/08/2026
-    Descrição: Lê um numero e calcula seu fatorial.
+    Descrição: Lê uma senha e verifica se está correta.
 '''
 
-a
+senhac = 1234
+tentativas = 1
+tenativaslim = 3
+
+senha = int(input("Digite a senha: "))
+
+while senha != senhac and tentativas < tenativaslim:
+    print("Senha incorreta")
+    tentativas += 1
+    senha = int(input("Digite a senha: "))
+    
+if senha == senhac:
+    print("Acesso liberado")
+else:
+    print("Senha incorreta")
