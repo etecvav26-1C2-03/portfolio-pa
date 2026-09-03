cdb7180ab0c7079109735ce727dc173f18c5f0d6
Gabriel Fernandes Barbarini <gabrielfernandesbarbarini@gmail.com>
2026-09-03 14:04:15 -0300

    Update main.py

--- 03-Bimestre/cauan-felipe-gabriel/snack-bar/gabriel/main.py
+++ 03-Bimestre/cauan-felipe-gabriel/snack-bar/gabriel/main.py
@@ -52,3 +52,66 @@ def register_product():
     save_data()
 
     print("Produto cadastrado com sucesso!")
+
+def list_products():
+    if len(produtos) == 0:
+        print ("Nenhum produto cadastrado. ")
+        return
+    
+    print("\n Produtos cadastrados ")
+    for produto in produtos:
+        print(f"Código: {produto['codigo']}")
+        print(f"Nome: {produto['nome']}")
+        print(f"Preço: R$ {produto['preco']:.2f}")
+        print(f"Estoque: {produto['estoque']}")
+        print("-" * 30)
+
+def find_product_by_code(codigo):
+    for produto in produtos:
+        if produto["codigo"] == codigo:
+            return produto
+    return None
+
+def make_order():
+    if len(produtos) == 0:
+        print("Nenhum produto cadastrado.")
+        return
+
+    cliente_nome = input("Nome do cliente: ")
+
+    list_products()
+
+    codigo = input("Digite o código do produto: ")
+    produto = find_product_by_code(codigo)
+
+    if produto is None:
+        print("Produto não existe.")
+        return
+
+    quantidade = int(input("Quantidade desejada: "))
+
+    if quantidade <= 0:
+        print("Quantidade inválida.")
+        return
+
+    if quantidade > produto["estoque"]:
+        print("Estoque insuficiente.")
+        return
+    
+    total = quantidade * produto["preco"]
+    produto["estoque"] -= quantidade
+
+    pedido = {
+        "cliente_nome": cliente_nome,
+        "produto_codigo": produto["codigo"],
+        "produto_nome": produto["nome"],
+        "quantidade": quantidade,
+        "total": total
+    }
+
+    pedidos.append(pedido)
+    save_data()
+
+    print("Pedido realizado com sucesso ")
+    print(f"Total: R$ {total:.2f}")
+
